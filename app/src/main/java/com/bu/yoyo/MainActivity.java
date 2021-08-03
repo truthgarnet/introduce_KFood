371615bac72ecc4460deb44ea7cd9a00d19fc748
truthgarnet <dmstj105@gmail.com>
2021-08-03 19:25:06 +0900

    splash
    Plus intro splash

--- app/src/main/java/com/bu/yoyo/MainActivity.java
+++ app/src/main/java/com/bu/yoyo/MainActivity.java
@@ -1,8 +1,6 @@
 package com.bu.yoyo;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.ActionBar;
@@ -19,14 +17,6 @@ public class MainActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
-        bnt1 = (Button) findViewById(R.id.btn0);
 
-        bnt1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), Map.class);
-                startActivity(intent);
-            }
-        });
     }
 }
---
+++ app/src/main/java/com/bu/yoyo/Splash.java
@@ -0,0 +1,34 @@
+package com.bu.yoyo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.spalsh);
+        //로딩화면 시작.
+        Loadingstart();
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+    }
+
+    private void Loadingstart() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), Map.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+
+}
